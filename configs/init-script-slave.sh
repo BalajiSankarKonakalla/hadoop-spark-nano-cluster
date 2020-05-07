a6a52ad7091db58132e168c775b68eba52082057
Balaji <balajisankarkonakalla@gmail.com>
2020-05-07 21:34:39 +0530

    <Balaji> change order in slave entrypoint shell script

--- configs/init-script-slave.sh
+++ configs/init-script-slave.sh
@@ -1,7 +1,7 @@
 sudo /etc/init.d/ssh start
-echo 'Y' | hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
+echo 'Y' | hdfs namenode -format
 while true; do        
     sleep 50
 done
